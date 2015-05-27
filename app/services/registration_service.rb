20be5f04f254bf87b473caecfe2b665bbebde8eb
David BF <burretfresket@gmail.com>
2015-05-27 11:13:56 +0200

    Unhound

--- app/services/registration_service.rb
+++ app/services/registration_service.rb
@@ -1,6 +1,6 @@
 class RegistrationService
   def from_invitation(email, invitation_token, user_data = {})
-    invitation = Invitation.find_by(email: email, token: invitation_token)
+    Invitation.find_by(email: email, token: invitation_token)
     user = User.create({ email: email }.merge(user_data))
     user
   rescue Mongoid::Errors::DocumentNotFound
