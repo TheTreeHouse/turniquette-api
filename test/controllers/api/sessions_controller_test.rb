7e0d7876a460078a0335642c2b4787568e898960
GpoNsU <simpons@gmail.com>
2015-05-31 02:06:48 +0200

    Proposal of tests refactorization

--- test/controllers/api/sessions_controller_test.rb
+++ test/controllers/api/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class Api::SessionsControllerTest < ActionController::TestCase
   def setup
-    User.create(email: 'johndoe@example.com', password: 'secret')
+    @user = User.create(email: 'johndoe@example.com', password: 'secret')
   end
 
   def teardown
@@ -14,33 +14,29 @@ class Api::SessionsControllerTest < ActionController::TestCase
     get :create, email: user.email, password: 'secret'
 
     assert_response :success
-    assert_not_nil user.reload.auth_token
+    assert_not_empty user.reload.auth_token
     assert_equal response.body, '{"session":{"success":true,"email":"johndoe@example.com","auth_token":"' + user.auth_token + '"}}'
   end
 
   test 'login fail' do
-    user = User.first
-    get :create, email: user.email, password: 'wrong-password'
+    get :create, email: @user.email, password: 'wrong-password'
 
     assert_response :success
     assert_equal response.body, '{"session":{"success":false}}'
   end
 
   test 'logout success' do
-    user = User.first
-    get :destroy, email: user.email, auth_token: user.auth_token
+    get :destroy, email: @user.email, auth_token: @user.auth_token
 
     assert_response :success
     assert_equal response.body, '{"session":{"success":true,"email":"johndoe@example.com","auth_token":null}}'
   end
 
   test 'logout fail' do
-    user = User.first
-    get :destroy, email: user.email, auth_token: 'wrong-token'
+    get :destroy, email: @user.email, auth_token: 'wrong-token'
 
     assert_response :success
-    assert_not_nil user.auth_token
+    assert_not_nil @user.auth_token
     assert_equal response.body, '{"session":{"success":false,"message":"Authentication error"}}'
   end
 end
-
--- test/services/registration_service_test.rb
+++ test/services/registration_service_test.rb
@@ -2,24 +2,17 @@ require 'test_helper'
 
 class RegistrataionServiceTest < ActiveSupport::TestCase
   def setup
-    Invitation.create(email: 'johndoe@example.com')
-  end
-
-  def teardown
-    Invitation.delete_all
-    User.delete_all
+    @invitation = Invitation.create(email: 'johndoe@example.com')
   end
 
   test 'with valid invitation, creates a user' do
-    invitation = Invitation.first
-    user_data = { email: invitation.email, password: 'my-password' }
-    assert_equal RegistrationService.new.from_invitation(invitation.email, invitation.token, user_data).class, User
+    user_data = { email: @invitation.email, password: 'my-password' }
+    assert_equal RegistrationService.new.from_invitation(@invitation.email, @invitation.token, user_data).class, User
   end
 
   test 'with NOT valid invitation token, do nothing' do
-    invitation = Invitation.first.tap { |item| item.token = 'faked-token' }
-    user_data = { email: invitation.email, password: 'my-password' }
-    assert_equal RegistrationService.new.from_invitation(invitation.email, invitation.token, user_data), nil
+    user_data = { email: @invitation.email, password: 'my-password' }
+    assert_equal RegistrationService.new.from_invitation(@invitation.email, 'faked-token', user_data), nil
   end
 
   test 'with NOT valid invitation, do nothing' do
