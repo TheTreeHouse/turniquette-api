d1acc023375ca070aaf041c235d7d9eb5e0f2ed2
scruti <pringaohowto@gmail.com>
2015-06-10 11:01:35 +0200

    Validating presence and uniqueness of User's email

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def teardown
+    User.delete_all
+  end
+
+  test 'requires email' do
+    user = User.new
+    assert_not user.valid?
+    assert_equal ["can't be blank"], user.errors[:email]
+  end
+
+  test 'does not allow more than one user with same email' do
+    user = User.create(email: 'homer@simpsons.com')
+    user_duplicated = User.new(email: 'homer@simpsons.com')
+    assert_not user_duplicated.valid?
+    assert_equal ["is already taken"], user_duplicated.errors[:email]
+  end
+end
