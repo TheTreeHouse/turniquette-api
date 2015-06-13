780a2c9948eafab06e4f435a5443c85734cab6d3
David BF <burretfresket@gmail.com>
2015-06-13 18:52:18 +0200

    Use assert_instance_of

--- test/services/registration_service_test.rb
+++ test/services/registration_service_test.rb
@@ -14,7 +14,7 @@ class RegistrationServiceTest < ActiveSupport::TestCase
   end
 
   test 'with valid invitation, creates a user' do
-    assert_equal RegistrationService.new.from_invitation(@invitation.email, @invitation.token, @user_data).class, User
+    assert_instance_of User, RegistrationService.new.from_invitation(@invitation.email, @invitation.token, @user_data)
   end
 
   test 'with valid invitation & existing user, does not create a new user' do
